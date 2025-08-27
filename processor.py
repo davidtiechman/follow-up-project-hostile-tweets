22738cf1518515732393bd756550e05e6e373cde
Knobloch Moshe <moskno1@gmail.com>
2025-08-27 14:35:17 +0300

    I have completed all the functions for initial data cleaning and finally return a dataframe with 2 more columns clean_data and list_words.

--- processor.py
+++ processor.py
@@ -1,12 +1,7 @@
-from nltk.sentiment import vader
-import os
 import string
-import re
-from collections import Counter
-import nltk
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
-class CleanText():
+from  nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+class InitialCleanText():
     def __init__(self):
         self.df = ''
         # self.df:
@@ -15,44 +10,27 @@ class CleanText():
         self.df['clene_data'] = self.df['Text'].str.translate(str.maketrans('', '', string.punctuation))
         return self.df
     def remove_spaces(self):
-        self.df['clene_data'] = self.df['Text'].str.replace(r'[^A-Za-z-9\s]', '', regex=True)
+        self.df['clean_text'] = self.df['Text'].str.replace(r'[^A-Za-z0-9\s]', '', regex=True)
         return self.df
     def remove_extra_whitespace(self):
-        pass
-    def remove_stopwords(self):
-        pass
+        self.df['clean_text'] = self.df['Text'].str.translate(r'\s+', ' ', regex=True).str.strip()
+        return self.df
     def convert_text_to_lowercase(self):
+        self.df['clean_text'].aplly(lambda x: x.lower())
+        return self.df
+    def division_text(self):
+        self.df['text_division'] = self.df['clean_text'].split()
+        return self.df
+    def remove_stopwords(self):
+        list_of_stopwords = stopwords.words('english')
+        self.df['list_words'].apply(lambda x:[word for word in x if word not in list_of_stopwords])
         pass
     def find_root_of_word(self):
-        pass
-
-    def rarest_word(self):
-        self.df['array_text'] = self.df['Text'].str.split(" ")
-        self.df['rarest_word'] = self.df['array_text'].apply(lambda words: Counter(words).most_common()[-1][0])
+        ps = PorterStemmer()
+        self.df['list_words'].apply(lambda x:[ps.stem(word) for word in x])
         return self.df
-
-    def emotion_text(self):
-        nltk.download('vader_lexicon')  # Compute sentiment labels
-        # tweet = self.df['Text'].loc[0]
-        self.df['score'] = self.df['Text'].apply(lambda text: vader.SentimentIntensityAnalyzer().polarity_scores(str(text))['compound'])
-        self.df['type_text'] = self.df['score'].apply(lambda s: 'positive' if s >= 0.5 else ('negative' if s <= -0.5 else 'neutral'))
-        self.df.drop(columns=['score'], inplace=True)
-        return self.df
-
-    def find_weapon_name(self):
-        self.df['weapons_detected'] = self.df.apply(self.lop_of_array_weapons, axis=1)
-        return self.df
-    def lop_of_array_weapons(self,row):
-        arr_weapon = []
-        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        file_path = os.path.join(BASE_DIR, 'data', 'weapon_list.txt')
-
-        with open(file_path, 'r') as file:
-            arr_weapon = file.read().split('\n')
-        for w in arr_weapon:
-            if w in row['array_text']:
-                return w
-        return ""
+    def retrons_to_string(self):
+        self.df['clean_text'] = " ".join(self.df['list_words'])
 
 
 
